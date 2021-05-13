01f0c3b534e13fc9ca2b809df039bc51181e72ce
Héctor Hugo Moreira <hector.moreirac@gmail.com>
2021-05-13 18:55:51 -0500

    information

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,49 +1,75 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
 import '../styles/components/Information.css';
 
-const Information = () => {
+const Information = ({ history }) => {
+    const { state, addToBuyer } = useContext(AppContext);
+    const form = useRef(null);
+    const { cart } = state;
+
+    const handleSubmit = () => {
+        const formData = new FormData(form.current);
+        const buyer = {
+            'name': formData.get('name'),
+            'email': formData.get('email'),
+            'address': formData.get('address'),
+            'apto': formData.get('apto'),
+            'city': formData.get('city'),
+            'country': formData.get('country'),
+            'state': formData.get('state'),
+            'cp': formData.get('cp'),
+            'phone': formData.get('phone'),
+        }
+        addToBuyer(buyer);
+    }
+
     return (
         <div className="Information">
             <div className="Information-content">
                 <div className="Information-head">
-                    <h2>Información de contacto:</h2>
+                    <h2>Informacion de contacto:</h2>
                 </div>
                 <div className="Information-form">
-                    <form action="">
+                    <form ref={form}>
                         <input type="text" placeholder="Nombre completo" name="name" />
-                        <input type="text" placeholder="Correo electrónico" name="email" />
-                        <input type="text" placeholder="Dirección" name="address" />
-                        <input type="text" placeholder="Dpto" name="dpto" />
+                        <input type="text" placeholder="Correo Electronico" name="email" />
+                        <input type="text" placeholder="Direccion" name="address" />
+                        <input type="text" placeholder="Apto" name="apto" />
                         <input type="text" placeholder="Ciudad" name="city" />
                         <input type="text" placeholder="Pais" name="country" />
-                        <input type="text" placeholder="Provincia" name="state" />
-                        <input type="text" placeholder="Código postal" name="cp" />
-                        <input type="text" placeholder="Teléfono" name="phone" />
+                        <input type="text" placeholder="Estado" name="state" />
+                        <input type="text" placeholder="Codigo postal" name="cp" />
+                        <input type="text" placeholder="Telefono" name="phone" />
                     </form>
                 </div>
                 <div className="Information-buttons">
                     <div className="Information-back">
-                        Regresar
-                    </div>
-                    <div className="Information-net">
-                        <Link to="/checkout/payment">
-                            Pagar
+                        <Link to="/checkout">
+                            Regresar
                         </Link>
+                    </div>
+                    <div className="Information-next">
+                        <button type="button" onClick={handleSubmit}> Pagar </button>
                     </div>
                 </div>
             </div>
             <div className="Information-sidebar">
                 <h3>Pedido:</h3>
-                <div className="Information-item">
-                    <div className="Information-element">
-                        <h4>ITEM name</h4>
-                        <span>$10</span>
+                {cart.map((item) => (
+                    <div className="Information-item" key={item.title}>
+                        <div className="Information-element">
+                            <h4>{item.title}</h4>
+                            <span>
+                                $
+                                {item.price}
+                            </span>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     );
-};
+}
 
 export default Information;
